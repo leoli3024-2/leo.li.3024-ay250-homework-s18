b2aca27b48740cac149dc53a84b4b567496cf05e
Leo Li <leo.li.3024@gmail.com>
2018-04-17 17:06:50 -0700

    modified test file

--- hw_8/hw8tests.py
+++ hw_8/hw8tests.py
@@ -3,10 +3,10 @@ import bib_server
 
 class TestHelper(unittest.TestCase):
 	def test_author(self):
-		self.assertEqual(bib_server.get_author_list('&Francis&, John'),'John Francis')
+		self.assertEqual(bib_server.get_author_list("{{Leavitt}, H.~S.}"),"H. S. Leavitt")
 
 	def second_test(self):
-		self.assertEqual(bib_server.get_author_list('&D.&, L. E.'),'L. E. D.')
+		self.assertEqual(bib_server.get_author_list("{{Pickering}, E.~C.}"),'E. C. Pickering')
 
 if __name__ == '__main__':
 	unittest.main()
